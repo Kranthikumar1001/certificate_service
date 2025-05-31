afe51ea808fc4c4220dcea415f4b19abf67dd7da
Kranthikumar1001 <forsomethingsneeded@gmail.com>
2025-05-31 18:36:15 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,12 +1,12 @@
 import smtplib
 from email.message import EmailMessage
 
-def send_certificate_email(receiver_email, subject, body, pdf_path, sender_email, sender_password):
+def send_email(receiver_email, pdf_path):
     msg = EmailMessage()
-    msg["Subject"] = subject
-    msg["From"] = sender_email
+    msg["Subject"] = "Your Participation certificate"
+    msg["From"] = "ditchrabbitsega@gmail.com"
     msg["To"] = receiver_email
-    msg.set_content(body)
+    #msg.set_content(body)
 
     # Attach PDF
     with open(pdf_path, "rb") as f:
@@ -16,5 +16,5 @@ def send_certificate_email(receiver_email, subject, body, pdf_path, sender_email
 
     # Send email via Gmail SMTP
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-        smtp.login(sender_email, sender_password)
+        smtp.login(sender_email, "Kr@nthi!001")
         smtp.send_message(msg)
